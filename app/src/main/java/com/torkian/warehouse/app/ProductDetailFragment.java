a51ecc21d22ecc7fdde6f2ebb6ab59011d1ac9ef
Brad Torkian <b.torkian@me.com>
2014-04-19 18:55:35 -0400

    View Menu

--- app/src/main/java/com/torkian/warehouse/app/ProductDetailFragment.java
+++ app/src/main/java/com/torkian/warehouse/app/ProductDetailFragment.java
@@ -1,15 +1,22 @@
 package com.torkian.warehouse.app;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.torkian.warehouse.app.product.WarehouseContent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * A fragment representing a single Product detail screen.
@@ -40,6 +47,7 @@ public class ProductDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         if (getArguments().containsKey(ARG_ITEM_ID)) {
             // Load the dummy content specified by the fragment
             // arguments. In a real-world scenario, use a Loader
@@ -52,6 +60,7 @@ public class ProductDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_product_detail, container, false);
+
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
@@ -66,6 +75,9 @@ public class ProductDetailFragment extends Fragment {
                 ((TextView)rootView.findViewById(R.id.textView2)).setVisibility(View.VISIBLE);
                 ((TextView)rootView.findViewById(R.id.textView3)).setVisibility(View.VISIBLE);
                 ((Button)rootView.findViewById(R.id.button)).setVisibility(View.VISIBLE);
+                ((ListView)rootView.findViewById(R.id.listView)).setVisibility(View.INVISIBLE);
+
+
 
             }else if (mItem.content.equalsIgnoreCase("View")){
                 ((EditText) rootView.findViewById(R.id.editText)).setVisibility(View.INVISIBLE);
@@ -75,6 +87,22 @@ public class ProductDetailFragment extends Fragment {
                 ((TextView) rootView.findViewById(R.id.textView2)).setVisibility(View.INVISIBLE);
                 ((TextView) rootView.findViewById(R.id.textView3)).setVisibility(View.INVISIBLE);
                 ((Button)rootView.findViewById(R.id.button)).setVisibility(View.INVISIBLE);
+                ((ListView)rootView.findViewById(R.id.listView)).setVisibility(View.VISIBLE);
+                ListView listView = (ListView) rootView.findViewById(R.id.listView);
+                String[] values = new String[] { "Android List View",
+                        "Adapter implementation",
+                        "Simple List View In Android",
+                        "Create List View Android",
+                        "Android Example",
+                        "List View Source Code",
+                        "List View Array Adapter",
+                        "Android Example List View"
+                };
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                        android.R.layout.simple_list_item_1, android.R.id.text1, values);
+                listView.setAdapter(adapter);
+
+
 
             }
             ((TextView) rootView.findViewById(R.id.product_detail)).setText(mItem.content);
@@ -82,4 +110,5 @@ public class ProductDetailFragment extends Fragment {
 
         return rootView;
     }
+
 }
